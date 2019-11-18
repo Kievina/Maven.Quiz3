f3b54ec283137dcf6e01e371201a41fd01944a88
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-18 15:05:09 -0500

    fundamentals tests pass

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -5,6 +5,29 @@ package rocks.zipcode.io.quiz3.fundamentals;
  */
 public class PigLatinGenerator {
     public String translate(String str) {
-        return null;
+        String[] stringArray = str.split(" ");
+        StringBuilder builder = new StringBuilder();
+        String result = "";
+        Integer indexFirstVowel = -1;
+
+        for (int i = 0; i < stringArray.length; i++) {
+            for (int j = 0; j < stringArray[i].length(); j++) {
+                if (VowelUtils.isVowel(stringArray[i].charAt(j))) {
+                    indexFirstVowel = j;
+                    break;
+                }
+            }
+            if (indexFirstVowel == 0)
+                stringArray[i] = stringArray[i] + "way";
+            else if (indexFirstVowel > 0)
+                stringArray[i] = stringArray[i].substring(indexFirstVowel, stringArray[i].length()) +
+                        stringArray[i].substring(0, indexFirstVowel) + "ay";
+            else
+                stringArray[i] = stringArray[i] + "ay";
+            builder.append(stringArray[i] + " ");
+        }
+
+        builder.deleteCharAt(builder.length() - 1);
+        return builder.toString();
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
@@ -5,19 +5,32 @@ package rocks.zipcode.io.quiz3.fundamentals;
  */
 public class VowelUtils {
     public static Boolean hasVowels(String word) {
-        return null;
+//        word = word.toLowerCase();
+        for (int i = 0; i < word.length(); i++) {
+            if (isVowel(word.charAt(i)))
+                return true;
+        }
+        return false;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+        Integer index = -1;
+        word = word.toLowerCase();
+        for (int i = 0; i < word.length(); i++) {
+            if (isVowel(word.charAt(i))) {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        return null;
+        return isVowel(word.charAt(0));
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+        return Character.toString(character).matches("[aeiouAEIOU]");
     }
 }
